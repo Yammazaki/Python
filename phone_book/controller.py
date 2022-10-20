a572331afc3bc1bcb3beb56b8cf3f12c09a1b2df
Dmitry V <faustt@bk.ru>
2022-10-20 18:03:49 +0300

    делали вместе с Алексеем, Анной и Николаем

---
+++ phone_book/controller.py
@@ -0,0 +1,27 @@
+import func
+import view 
+
+def directory():
+     oper = int(input('1 - Добавить контакт \n2 - Удалить контакт \n3 - Найти котакт \n4 - Показать все контакты \n5 - Удаление по поиску \n6 - Выход \n Введите, что вы хотите сделать 1-6: '))
+     while oper <= 6:
+          if oper == 1:
+               func.get_add()
+          elif oper == 2:
+               func.get_del()
+          elif oper == 3:
+               func.get_find()
+          elif oper == 4:
+               func.get_see()
+          elif oper == 5:
+               func.get_del_find()
+          if oper == 6:
+               print('Спасибо, что выбрали наш Продукт')
+               break
+          oper = int(input('1 - Добавить контакт \n2 - Удалить контакт \n3 - Найти котакт \n4 - Показать все контакты \n5 - Удаление по поиску \n6 - Выход \n Введите, что вы хотите сделать 1-6: '))
+            
+
+    
+    
+      
+
+
---
+++ phone_book/func.py
@@ -0,0 +1,79 @@
+import view
+import os
+
+def get_add():
+    list = ['Фамилию: ','Имя: ','номер телефона: ','коментарий']
+    data = view.get_input(list)
+    with open('people.csv', 'a', encoding = 'UTF-8') as file:
+        file.writelines(
+            f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
+    with open('people.txt', 'a', encoding = 'UTF-8') as file:
+        file.writelines(
+            f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
+
+def get_see():
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        list = []
+        list = ''.join(file.readlines())
+        print(list)
+
+def get_find():
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data_see = []
+        data = file.readlines()
+        text = input('Введите слово для поиска: \n')
+        for i in range(len(data)):
+            data[i] = ''.join(data[i])
+            row = []
+            row = data[i]
+            if text in row:
+                print(f'N {i+1} : {row}')
+        
+def get_del():
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data = file.readlines()
+        print('-'*100)
+        for i in range(len(data)):
+            print(f'№ {i+1} |{data[i]}')
+    number = int(input('Введите номер контакта для удаления: '))
+    n = number-1
+    data.pop(n)
+    print('Вы удалили контакт')
+    for i in range(len(data)):
+            print(f'№ {i+1} |{data[i]}')
+    with open('people.txt', 'w', encoding = 'UTF-8') as file:
+        file.writelines(data)
+    # with open('people.csv', 'w', encoding = 'UTF-8') as file:
+    #     file.writelines(data)
+
+def get_del_find():
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data_see = []
+        data = file.readlines()
+        text = input('Введите слово для поиска: \n')
+        for i in range(len(data)):
+            data[i] = ''.join(data[i])
+            row = []
+            row = data[i]
+            if text in row:
+                print(f'N {i+1} удалить? : {row}')
+        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет \nВвод: ')) 
+        if oper == 1: 
+            os.remove('people.txt')       
+            for i in range(len(data)):
+                data[i] = ''.join(data[i])
+                row = []
+                row = data[i]
+                data_new = []
+                if text not in row:        
+                    data_new.append(row)
+                    data_new = ' '.join(' '.join(''.join(data_new).split()).split())
+                with open('people.txt', 'a', encoding = 'UTF-8') as file:
+                    file.writelines(f'{data_new} \n')
+                # with open('people.csv', 'a', encoding = 'UTF-8') as file:
+                #     file.writelines(data)
+        if oper == 2: 
+            get_see() 
